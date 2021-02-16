20072ac2e8bfe465b0230312f47518c08a3d15a8
Ivan <ivaan.lg.92@gmail.com>
2021-02-15 18:42:25 -0600

    [main] Hans translator v1 complete

---
+++ dist/js/main.js
@@ -0,0 +1,24 @@
+import translate from './translator.js';
+
+const main = () => {
+    const input = document.querySelector('#morse-code');
+    const chatContainer = document.querySelector('.chat-container')
+    input.addEventListener('keydown', e => {
+        if(e.key == 'Enter'){
+            e.preventDefault();
+            chatContainer.appendChild(paintChat({myMessage: true, text:`${input.value}`}))
+            chatContainer.appendChild(paintChat({myMessage: false, text:`Translates to: \n <b>${translate(input.value)}</b>`}))
+            chatContainer.scrollTo(0, chatContainer.scrollHeight);
+            input.value = '';
+        }
+    } )
+}
+
+const paintChat = ({myMessage, text}) => {
+    const html = `<div class="message ${ myMessage ? 'my-message' : 'your-message'}"> <p>${text}</p></div>`
+    const element = document.createElement('div');
+    element.innerHTML = html;
+    return element.firstElementChild; 
+}
+
+main();
---
+++ dist/js/translator.js
@@ -0,0 +1,17 @@
+import { default as morseCodeFile } from './alphabet.js';
+
+function translate(morseCodeString){
+    let translatedString= ``;
+    const words = morseCodeString.split('/');
+    const translation = words.map( word => {
+        const letters = word.split(' ');
+        const translatedWord = letters.filter( letter => letter != '').map( letter =>{
+            translatedString += `${morseCodeFile[letter]}`
+        })
+        translatedString += ' ';
+        return translatedWord;
+    })
+    return translatedString;
+}
+
+export default translate;
